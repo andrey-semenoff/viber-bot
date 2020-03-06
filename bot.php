94403c2fb085d62ca1d6a6c453e6cf57729ed196
Andrey Semenov <andrey.semenoff@gmail.com>
2020-03-07 01:34:34 +0200

    Init

---
+++ bot.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("../vendor/autoload.php");
+
+use Viber\Bot;
+use Viber\Api\Sender;
+
+$apiKey = '4b2c8ae9d8e7dd17-a4ddce61389be519-78f5da28d7dc8cd3';
+
+// reply name
+$botSender = new Sender([
+    'name' => 'Renessans bot',
+    'avatar' => 'https://developers.viber.com/img/favicon.ico',
+]);
+
+try {
+    $bot = new Bot(['token' => $apiKey]);
+    $bot
+    ->onConversation(function ($event) use ($bot, $botSender) {
+        // this event fires if user open chat, you can return "welcome message"
+        // to user, but you can't send more messages!
+        return (new \Viber\Api\Message\Text())
+            ->setSender($botSender)
+            ->setText("Can i help you?");
+    })
+    ->onText('|whois .*|si', function ($event) use ($bot, $botSender) {
+        // match by template, for example "whois Bogdaan"
+        $bot->getClient()->sendMessage(
+            (new \Viber\Api\Message\Text())
+            ->setSender($botSender)
+            ->setReceiver($event->getSender()->getId())
+            ->setText("I do not know )")
+        );
+    })
+    ->run();
+} catch (Exception $e) {
+    // todo - log exceptions
+}
---
+++ setup.php
@@ -0,0 +1,15 @@
+<?php
+require_once("../vendor/autoload.php");
+
+use Viber\Client;
+
+$apiKey = '4b2c8ae9d8e7dd17-a4ddce61389be519-78f5da28d7dc8cd3'; // <- PLACE-YOU-API-KEY-HERE
+$webhookUrl = 'https://renessans-viber-bot.herokuapp.com/bot.php'; // <- PLACE-YOU-HTTPS-URL
+
+try {
+    $client = new Client([ 'token' => $apiKey ]);
+    $result = $client->setWebhook($webhookUrl);
+    echo "Success!\n";
+} catch (Exception $e) {
+    echo "Error: ". $e->getError() ."\n";
+}
