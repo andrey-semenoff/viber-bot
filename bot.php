d7d960ebf693188e7ee8f6f9f7a6f68a9d68bfcc
Andrey Semenov <andrey.semenoff@gmail.com>
2020-03-07 02:57:40 +0200

    Test 2: get reply

--- bot.php
+++ bot.php
@@ -18,11 +18,35 @@ try {
     $bot = new Bot(['token' => $apiKey]);
     $bot
     ->onConversation(function ($event) use ($bot, $botSender) {
+        $user = $bot->getClient()->getAccountInfo()->getData();
+        $user2 = $bot->getClient()->getAccountInfo();
+        var_dump($user['name']);
+        var_dump($user2['name']);
         // this event fires if user open chat, you can return "welcome message"
         // to user, but you can't send more messages!
+        $msg = 'Здравствуйте';
+        if( $user['name'] ) {
+            $msg .= ', ' . $user['name'];
+        } else {
+            $msg .= '!';
+        }
         return (new \Viber\Api\Message\Text())
             ->setSender($botSender)
-            ->setText("Can i help you?");
+            ->setText($msg);
+    })
+    ->onText('/вода\s*(\d+)/i', function ($event) use ($bot, $botSender) {
+        $reply = $event->message->text;
+        var_dump($reply);
+        $answer = "Извините, я не могу разобрать ваши показания воды! Попробуйте еще раз!";
+        if( $reply ) {
+            $answer = "Спасибо! Приняты показания воды: '123' для квартиры: '1' - $reply";
+        }
+        $bot->getClient()->sendMessage(
+            (new \Viber\Api\Message\Text())
+            ->setSender($botSender)
+            ->setReceiver($event->getSender()->getId())
+            ->setText($answer)
+        );
     })
     ->onText('/whois .*/si', function ($event) use ($bot, $botSender) {
         // match by template, for example "whois Bogdaan"
@@ -33,25 +57,24 @@ try {
             ->setText("I do not know )")
         );
     })
-    ->onText('/.*/i', function ($event) use ($bot, $botSender) {
-        // match by template, for example "whois Bogdaan"
-        $bot->getClient()->sendMessage(
-            (new \Viber\Api\Message\Text())
-            ->setSender($botSender)
-            ->setReceiver($event->getSender()->getId())
-            ->setText("Select action")
-            ->setKeyboard(
-                (new \Viber\Api\Keyboard)
-                ->setButtons([
-                    (new \Viber\Api\Keyboard\Button())
-                    ->setColumns(6)
-                    ->setText('Click me')
-                    ->setActionType('reply')
-                    ->setActionBody('Clicked!')
-                ])
-            )
-        );
-    })
+//    ->onText('/.*/i', function ($event) use ($bot, $botSender) {
+//        // match by template, for example "whois Bogdaan"
+//        $bot->getClient()->sendMessage(
+//            (new \Viber\Api\Message\Text())
+//            ->setSender($botSender)
+//            ->setReceiver($event->getSender()->getId())
+//            ->setText("Select action")
+//            ->setKeyboard(
+//                (new \Viber\Api\Keyboard)
+//                ->setButtons([
+//                    (new \Viber\Api\Keyboard\Button())
+//                    ->setColumns(6)
+//                    ->setText('Click me')
+//                    ->setActionBody('Clicked!')
+//                ])
+//            )
+//        );
+//    })
     ->run();
 } catch (Exception $e) {
     // todo - log exceptions
