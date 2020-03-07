a8a40f49b78d41a003184c30f6a175afc8ee6ea2
Andrey Semenov <andrey.semenoff@gmail.com>
2020-03-07 02:18:01 +0200

    Test 1: add new feature to bot

--- bot.php
+++ bot.php
@@ -10,7 +10,8 @@ $apiKey = '4b2c8ae9d8e7dd17-a4ddce61389be519-78f5da28d7dc8cd3';
 // reply name
 $botSender = new Sender([
     'name' => 'Renessans bot',
-    'avatar' => 'https://developers.viber.com/img/favicon.ico',
+    // 'avatar' => 'https://developers.viber.com/img/favicon.ico',
+    'avatar' => 'https://miro.medium.com/fit/c/256/256/1*Ro9Yq4D5W85QagF2rFTivw.png',
 ]);
 
 try {
@@ -30,6 +31,25 @@ try {
             ->setSender($botSender)
             ->setReceiver($event->getSender()->getId())
             ->setText("I do not know )")
+        );
+    })
+    ->onText('.*', function ($event) use ($bot, $botSender) {
+        // match by template, for example "whois Bogdaan"
+        $bot->getClient()->sendMessage(
+            (new \Viber\Api\Message\Text())
+            ->setSender($botSender)
+            ->setReceiver($event->getSender()->getId())
+            ->setText("Select action")
+            ->setKeyboard(
+                (new \Viber\Api\Keyboard)
+                ->setButtons([
+                    (new \Viber\Api\Keyboard\Button())
+                    ->setColumns(6)
+                    ->setText('Click me')
+                    ->setActionType('reply')
+                    ->setActionBody('Clicked!')
+                ])
+            )
         );
     })
     ->run();
